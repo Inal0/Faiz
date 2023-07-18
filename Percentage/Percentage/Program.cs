2a03b3eb4ba077074d2ad52e384d7f2404e922ce
Inal0 <138590266+Inal0@users.noreply.github.com>
2023-07-18 10:47:06 +0400

    Homework
    Percentage

---
+++ Percentage/Percentage/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Faiz deyerini qeyd edin");
+String faiz = Console.ReadLine();
+int[] num = { 5, 10, 15 };
+for (int i = 0; i < num.Length; i++)
+{
+	if (num[i] <20)
+	{
+
+	}
+}
